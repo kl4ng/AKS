826daee06b875ac6a257cd9085fc2c103370b836
kl4ng <klang2012@gmail.com>
2014-03-12 15:02:57 -0400

    added some base code

--- src/AKS.java
+++ src/AKS.java
@@ -1,4 +1,65 @@
+/* Kevin Lang
+ * Evan Dorundo
+ */
+
+import java.math.BigInteger;
+
 
 public class AKS {
-
+	
+	public static boolean isPrime(int num){
+		return isPrime(BigInteger.valueOf(num));
+	}
+	
+	
+	public static boolean isPrime(BigInteger n){
+		// (1)see if n = a^b 
+		if(isPowerOfInteger(n))
+			return false;
+		
+		//(2) find smallest r s.t. Or(n) > (log n)^2
+		BigInteger r = multOrder(n);
+		
+		//(3) see if 1 < gcd(a,n) < n for some a<=r
+		BigInteger a = BigInteger.valueOf(2);
+		BigInteger one = BigInteger.valueOf(1);
+		
+		for(; a.compareTo(r) <= 0; a.add(one)){
+			BigInteger gcd = gcd(a, r);
+			if(gcd.compareTo(one) > 0 && gcd.compareTo(n) < 0)
+				return false;
+		}
+		
+		//(4) if n <= r, we know its prime
+		if(n.compareTo(r) <= 0)
+			return true;
+		
+		//(5) 
+		
+		//(6) we know its not composite by this point
+		return true;
+	}
+	
+	private static boolean isPowerOfInteger(BigInteger num){
+		
+	}
+	
+	private static BigInteger multOrder(BigInteger num){
+		
+	}
+	
+	//basic euclidian gcd algorithm
+	private static BigInteger gcd(BigInteger a, BigInteger b){
+		while(!a.equals(b)){
+			if(a.compareTo(b) == 1)
+				a = a.subtract(b);
+			else
+				b = b.subtract(a);
+		}
+		return a;
+	}
+	
+	public static void main(String[] args){
+		
+	}
 }
